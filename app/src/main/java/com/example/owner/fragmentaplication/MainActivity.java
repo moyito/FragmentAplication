d9731c5435946f4c941108ed3f55e2e7f7afe106
moyito <mperez@nextgenerationcomputer.com>
2016-11-18 21:29:43 -0800

    initial commit by moyito 11-18-2016 9:28pm

--- app/src/main/java/com/example/owner/fragmentaplication/MainActivity.java
+++ app/src/main/java/com/example/owner/fragmentaplication/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.owner.fragmentaplication;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
-
+    ViewGroup container;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+        container = (ViewGroup) findViewById(R.id.container);
+    }
+
+    public void viewFragment1(View view) {
+        FirstFragment firstFragment = new FirstFragment();
+        getFragmentManager().beginTransaction().replace(R.id.container,firstFragment).commit();
+    }
+
+    public void viewFragment2(View view) {
+        SecondFragment secondFragment = new SecondFragment();
+        getFragmentManager().beginTransaction().replace(R.id.container,secondFragment).commit();
+    }
+
+    public void viewFragment3(View view) {
+        ThirdFragment thirdFragment = new ThirdFragment();
+        getFragmentManager().beginTransaction().replace(R.id.container,thirdFragment).commit();
     }
 }
